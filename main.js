8d50b3dc4b9da48338de33a73a074f4d4930aa6d
mmfiber <masa.03191996@gmail.com>
2021-04-17 10:11:47 +0900

    add base template and submit handler

---
+++ main.js
@@ -0,0 +1,15 @@
+"use strict"
+
+const warikanForm = document.getElementById("warikan_form")
+
+function onSubmitWarikanForm() {
+  // get warikan input
+  // validate input
+  // calculate warikan
+  // output result
+}
+
+function main() {
+  // create warikan form
+  warikanForm.addEventListener("submit", onSubmitWarikanForm)
+}
